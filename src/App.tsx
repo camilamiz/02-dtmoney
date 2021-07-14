7830d8120e494224806d8e6334e898fb61057221
Camila Mizumoto <camila.yuri.m@gmail.com>
2021-07-14 20:13:20 -0300

    add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
-import styled from 'styled-components';
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-
-  button{
-
-  }
-`;
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <Title>
-      Hello, world!
-      <button>Salvar</button>
-    </Title>
+    <div className="App">
+      <h1>
+        Hello, world!
+      </h1>
+      <GlobalStyle />
+    </div>
   );
 }
 
