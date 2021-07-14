ed92b16ea1592619dd14ff6153ffbeb6d4805c39
Camila Mizumoto <camila.yuri.m@gmail.com>
2021-07-14 20:16:43 -0300

    font styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -41,4 +41,13 @@ export const GlobalStyle = createGlobalStyle`
     opacity: 0.6;
     cursor: not-allowed;
   }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, strong {
+    font-weight: 600;
+  }
 `;
