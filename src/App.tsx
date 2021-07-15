04b885f3a24bb1e15c3dfd6568c9d44e2bf3cc23
Camila Mizumoto <camila.yuri.m@gmail.com>
2021-07-15 11:00:21 -0300

    change modal from hedert to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
+import Modal from 'react-modal';
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNeTransactioModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactioModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNeTransactioModal}/>
       <Dashboard />
+      <Modal
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactioModal}
+        >
+          <h2>Cadastrar transação</h2>
+        </Modal>
       <GlobalStyle />
     </>
   );
