fbbe128b9ac84ef5a92ff2e7422fcb7e6dc17420
Camila Mizumoto <camila.yuri.m@gmail.com>
2021-07-14 21:46:13 -0300

    adding miragejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-import { TransactionsTable } from "./components/TransactionsTable";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
